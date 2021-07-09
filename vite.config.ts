a3505d362a74fcf90fe26bf9119cc7652c7be284
John Darryl Pelingo <johndarrylpelingo@gmail.com>
2021-07-10 00:10:16 +0200

    chore: linting optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
 })
